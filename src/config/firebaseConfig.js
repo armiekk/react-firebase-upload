272ceb70340088dfe8efe23e9093b4b3a3806f65
Kampanart.k <kampanart.k@cdg.co.th>
2017-07-01 22:28:31 +0700

    change config

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 
 const config = {
-    apiKey: "AIzaSyC_H6v0KuJx1IpD1g3P5z-oTx_YRXc4iVg",
-    authDomain: "upload-example-ab58e.firebaseapp.com",
-    databaseURL: "https://upload-example-ab58e.firebaseio.com",
-    projectId: "upload-example-ab58e",
-    storageBucket: "upload-example-ab58e.appspot.com",
-    messagingSenderId: "865568879802"
+    apiKey: "yourCompleXapiKey",
+    authDomain: "upload-example.firebaseapp.com",
+    databaseURL: "https://upload-example.firebaseio.com",
+    projectId: "upload-example",
+    storageBucket: "upload-example.appspot.com",
+    messagingSenderId: "967468070501"
   };
 
 const firebaseApp = firebase.initializeApp(config);
